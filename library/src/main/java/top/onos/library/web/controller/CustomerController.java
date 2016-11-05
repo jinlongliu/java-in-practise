ae13791a8c18dfdb1b68dd18d78ae1121051c068
Liu Jinlong <nnuljl@gmail.com>
2016-11-05 21:27:20 +0800

    请求处理方法返回类型

--- library/src/main/java/top/onos/library/web/controller/CustomerController.java
+++ library/src/main/java/top/onos/library/web/controller/CustomerController.java
@@ -22,6 +22,18 @@ public class CustomerController {
         return "CustomerForm";
     }
 
+
+    /*请求处理方法可以返回的类型对象：
+    * 1.ModelAndView
+    * 2.Model
+    * 3.Map包含模型的属性
+    * 4.View
+    * 5.代表逻辑视图名的String
+    * 6.void
+    * 7.提供对Servlet的访问，以响应HTTP头部或者内容HttpEntity或ResponseEntity对象
+    * 8.Callable
+    * 9.DeferredResult
+    * 10.其它任意类型，Spring将其视作输出给View的对象模型*/
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public String deleteCustomer(HttpSession session) {
         //do something here
