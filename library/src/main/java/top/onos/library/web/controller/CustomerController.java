da064400fc644ba6a693af5eb1fc214c01c1a913
Liu Jinlong <nnuljl@gmail.com>
2016-11-05 21:09:10 +0800

    基于注解的处理方法。

--- library/src/main/java/top/onos/library/web/controller/CustomerController.java
+++ library/src/main/java/top/onos/library/web/controller/CustomerController.java
@@ -2,10 +2,19 @@ package top.onos.library.web.controller;
 
 import org.springframework.stereotype.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Liu on 2016/11/5.
  */
 @Controller
 public class CustomerController {
+
+    @RequestMapping(value = "/customer_input")
+    public String inputCustomer() {
+
+        //Todo
+
+        return "CustomerForm";
+    }
 }
