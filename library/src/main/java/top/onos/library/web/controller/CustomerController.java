e90670d3228f73f655ce229dcb046419ac92559a
Liu Jinlong <nnuljl@gmail.com>
2016-11-05 20:54:44 +0800

    新增注释方式声明的控制类

---
+++ library/src/main/java/top/onos/library/web/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package top.onos.library.web.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Liu on 2016/11/5.
+ */
+@Controller
+public class CustomerController {
+}
