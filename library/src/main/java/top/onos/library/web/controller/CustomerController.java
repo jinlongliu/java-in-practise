7b8b95385f3de22a15124037a86aa2e70560da51
Liu Jinlong <nnuljl@gmail.com>
2016-11-05 21:17:38 +0800

    新增处理方法

--- library/src/main/java/top/onos/library/web/controller/CustomerController.java
+++ library/src/main/java/top/onos/library/web/controller/CustomerController.java
@@ -3,11 +3,13 @@ package top.onos.library.web.controller;
 import org.springframework.stereotype.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Liu on 2016/11/5.
  */
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
     @RequestMapping(value = "/customer_input")
@@ -16,5 +18,11 @@ public class CustomerController {
         //Todo
 
         return "CustomerForm";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String deleteCustomer() {
+        //do something here
+        return "Success";
     }
 }
