a8bbd63438bb99e1939bd176574d853e284e0baf
Liu Jinlong <nnuljl@gmail.com>
2016-11-07 13:06:29 +0800

    集成jdbc template，domain类

---
+++ library/src/main/java/top/onos/library/web/domain/User.java
@@ -0,0 +1,30 @@
+package top.onos.library.web.domain;
+
+/**
+ * Created by Liu on 2016/11/7.
+ */
+public class User {
+    private String name;
+    private String address;
+
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
